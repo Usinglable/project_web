61867f761bbb9c054e038084f25cea3fc24d775c
qalqa <andreybas04@gmail.com>
2024-02-19 08:03:04 +0300

    Кривущая хуета, но работает
    а мб и нет, я не ебу

--- my-app/src/pages/TableApp.tsx
+++ my-app/src/pages/TableApp.tsx
@@ -1,5 +1,4 @@
 import { ColumnsType } from 'antd/es/table';
-import './App.css';
 import { Button, Table } from 'antd';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
@@ -23,33 +22,34 @@ const columns: ColumnsType<DataType> = [
   },
 ];
 
-function App() {
+const TableApp = () => {
 
   const LIMIT_LIST_SCHOOL: number = 10
 
   const [dataSource, setDataSource] = useState<DataType[]>();
-  
-  const [page, setPage] = useState<number>(1);
+
+  const [page, setPage] = useState<number>(0);
 
   let offset = page * LIMIT_LIST_SCHOOL
 
-  const getUniversity = async (page: number, limit: number, offset: number) => {
+  const getUniversity = async (limit: number, offset: number) => {
     const response = await axios.get(`http://universities.hipolabs.com/search?offset=${offset}&limit=${limit}`)
     setDataSource(response.data);
   }
 
 
   useEffect(() => {
-    getUniversity(page, LIMIT_LIST_SCHOOL, offset)
+    getUniversity(LIMIT_LIST_SCHOOL, offset)
   }, [offset])
+
   return (
     <>
       <Table dataSource={dataSource} columns={columns} pagination={false} />
       <Button onClick={() => setPage(page - 1)} disabled={!offset}>Назад</Button>
       <Button onClick={() => setPage(page + 1)}>Вперед</Button>
-      <span>Номер страницы: {page}</span>
+      <span>Номер страницы: {page + 1}</span>
     </>
   );
 }
 
-export default App;
+export default TableApp;
